bf2657fac34728f059789768adf86dcdde14c512
jjhonny <ojhonny1@hotmail.com>
2024-05-17 14:08:49 -0400

    feat: create table calls

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { TicketItem } from "./components/ticket";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -12,7 +14,39 @@ export default async function Dashboard() {
 
   return (
     <Container>
-      <h1>Pagina dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link
+            className="bg-blue-500 px-4 py-2 rounded text-white"
+            href="/dashboard/new"
+          >
+            Abrir chamado
+          </Link>
+        </div>
+
+        <table className="min-w-full my-4">
+          <thead>
+            <tr>
+              <th
+                className="font-medium text-left pl-1
+              "
+              >
+                Cliente
+              </th>
+              <th className="font-medium text-left hidden sm:block">
+                Data cadastro
+              </th>
+              <th className="font-medium text-left">Status</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TicketItem />
+            <TicketItem />
+          </tbody>
+        </table>
+      </main>
     </Container>
   );
 }
