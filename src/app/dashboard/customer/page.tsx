4e3eb8579ecc50e63064dbeeb444c74eba4b3800
jjhonny <ojhonny1@hotmail.com>
2024-05-18 17:35:26 -0300

    feat: create customer card for customer page

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { CardCustomer } from "./components/card";
 
 export default async function Customer() {
   const session = await getServerSession(authOptions);
@@ -12,10 +14,19 @@ export default async function Customer() {
 
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Meus clientes</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Meus clientes</h1>
+          <Link className="bg-blue-500 text-white px-4 py-2 rounded" href="/dashboard/customer/new">
+            Novo cliente
+          </Link>
         </div>
+
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
       </main>
     </Container>
   );
