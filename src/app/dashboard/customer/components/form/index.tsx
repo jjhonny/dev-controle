233b321874f1b8411e1d0f8949e292654d084f8c
jjhonny <ojhonny1@hotmail.com>
2024-05-20 09:34:42 -0400

    feat: makes form for new customer page

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -2,6 +2,7 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/input";
 
 const schema = z.object({
   name: z.string().min(1, "O campo nome é obrigatorio."),
@@ -35,10 +36,59 @@ export function NewCustomerForm() {
     resolver: zodResolver(schema),
   });
 
+  function handleRegisterCustomer(data: FormData) {
+    console.log(data);
+  }
+
   return (
-    <form>
-      <label>Nome completo:</label>
-      <input type="text" placeholder="Digite o nome completo" />
+    <form
+      className="flex flex-col mt-6"
+      onSubmit={handleSubmit(handleRegisterCustomer)}
+    >
+      <label className="mb-1 text-lg font-medium">Nome completo:</label>
+      <Input
+        type="text"
+        name="name"
+        placeholder="Digite o nome completo"
+        error={errors.name?.message}
+        register={register}
+      />
+      <section className="flex gap-2 my-2 flex-col sm:flex-row">
+        <div className="flex-1">
+          <label className="mb-1 text-lg font-medium">Telefone:</label>
+          <Input
+            type="text"
+            name="phone"
+            placeholder="Digite o telefone: (DD) 999999999"
+            error={errors.phone?.message}
+            register={register}
+          />
+        </div>
+        <div className="flex-1">
+          <label className="mb-1 text-lg font-medium">Email:</label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Digite o email"
+            error={errors.email?.message}
+            register={register}
+          />
+        </div>
+      </section>
+      <label className="mb-1 text-lg font-medium">Endereço completo:</label>
+      <Input
+        type="text"
+        name="address"
+        placeholder="Digite o endereço do cliente"
+        error={errors.address?.message}
+        register={register}
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 my-4 px-2 h-11 text-white font-bold rounded hover:bg-blue-600"
+      >
+        Cadastrar
+      </button>
     </form>
   );
 }
